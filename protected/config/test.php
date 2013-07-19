8a812c6c928b12a59871b1357624b8c6bcc77911
topazas <topazas@gmail.com>
2013-07-19 16:50:55 +0300

    travis adjusments

--- protected/config/test.php
+++ protected/config/test.php
@@ -1,17 +1,30 @@
 <?php
 
 return CMap::mergeArray(
-	require(dirname(__FILE__).'/main.php'),
-	array(
-		'components'=>array(
-			'fixture'=>array(
-				'class'=>'system.test.CDbFixtureManager',
-			),
-			/* uncomment the following to provide test database connection
-			'db'=>array(
-				'connectionString'=>'DSN for test database',
-			),
-			*/
-		),
-	)
+    require(dirname(__FILE__) . '/main.php'),
+    array(
+        'components' => array(
+            'fixture' => array(
+                'class' => 'system.test.CDbFixtureManager',
+            ),
+            'db' => array(
+                'connectionString' => 'mysql:host=localhost;dbname=newtimelist',
+                'emulatePrepare' => true,
+                'username' => 'travis',
+                'password' => 'dev',
+                'charset' => 'utf8',
+                'enableProfiling' => true,
+                'enableParamLogging' => true
+            ),
+            'log' => array(
+                'class' => 'CLogRouter',
+                'routes' => array(
+                    array(
+                        'class' => 'CFileLogRoute',
+                        'levels' => 'error, warning',
+                    ),
+                ),
+            ),
+        ),
+    )
 );
